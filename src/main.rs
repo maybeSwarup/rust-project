d24ea48f51cb92ece0d037092a39390595fda336
Swarup <swarup.suryawanshi@velotio.com>
2024-06-08 12:32:10 +0530

    random number

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,7 @@
-fn find_first_a(s: String) -> Option<usize> {
-    for (index, char) in s.chars().enumerate() {
-        if char == 'a' {
-            return Some(index);
-        }
-    }
-    return None
-}
+use rand::{Rng, thread_rng};
 
 fn main() {
-    let my_string = String::from("remun");
-    let res = find_first_a(my_string);
-
-    match res {
-        Some(index) => println!("The letter 'a' is found at index: {}", index),
-        None => println!("The letter 'a' is not foudn in the string."),
-    }
+    let mut rng = thread_rng();
+    let n: u32 = rng.gen();
+    println!("random number: {}", n);
 }
